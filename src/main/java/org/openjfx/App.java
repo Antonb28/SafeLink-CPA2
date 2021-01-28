8a57808b897a123adc8d7b33ed2da7a135b94510
Antonb28 <62889116+Antonb28@users.noreply.github.com>
2021-01-28 21:00:18 +0800

    Test

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("BoardBus"));
+        scene = new Scene(loadFXML("LogIn"));
         stage.setScene(scene);
         stage.show();
     }
